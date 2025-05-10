752e0f6fb8fe894ea65862b5bb304ee135eae2cf
Abioye Kazeem <112862410+AbioyeKazeem@users.noreply.github.com>
2025-05-10 09:53:17 +0100

    Update Footer.js updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://x.com/ExcellenceHome_"
+                href="#"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/excellencehome/"
+                href="#"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
